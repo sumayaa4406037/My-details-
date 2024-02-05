beb412127454c5d521cd63b63c410c340724922b
sumayaa4406037 <149380178+sumayaa4406037@users.noreply.github.com>
2024-02-05 20:42:00 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const my_details= {my_name :"Sumaya", my_age :"19" , my_dob :"07_12_2004" , father :"Taizul Islam", mother :"Shahida Begum"  };
+document.getElementById('name'). innerHTML = "My name is" + " " + my_details.my_name ;
+document.getElementById('age'). innerHTML = "My age" + " " + my_details.my_age ;
+document.getElementById('dob'). innerHTML = "My dob" + " " + my_details.my_dob ;
+document.getElementById('father'). innerHTML = "My father name is" + " " + my_details.father ;
+document.getElementById('mother'). innerHTML = "My mother name is" + " " + my_details.mother ;
